27bdf78c62dfa6802152b69f0d85b624156259bc
Ekaterina <katya@ekaterinas-mbp.home>
2026-05-15 02:55:18 +0200

    fix shopify env variable names

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -1,5 +1,5 @@
-const domain = process.env.SHOPIFY_STORE_DOMAIN!
-const storefrontAccessToken = process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN!
+const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN!
+const storefrontAccessToken = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_ACCESS_TOKEN!
 
 export async function shopifyFetch<T>(query: string, variables?: Record<string, unknown>): Promise<T> {
   const response = await fetch(`https://${domain}/api/2024-01/graphql.json`, {
